510479ee330edc2ea351bf54ea2a4f06a2a7a305
Antonello Provenzano <antonello@deveel.com>
2023-03-20 23:46:12 +0100

    Redesigning the data entity model

---
+++ src/Deveel.Repository.EntityModel/Data/ITenantDataEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Deveel.Data {
+	/// <summary>
+	/// Defines an entity that is owned by a tenant
+	/// </summary>
+	public interface ITenantDataEntity : IDataEntity {
+		/// <summary>
+		/// Gets the identifier of the tenant
+		/// </summary>
+		string TenantId { get; }
+	}
+}
